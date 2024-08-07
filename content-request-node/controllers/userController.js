ffc838869041f19c1369a976031790732052667c
Barcodehub <alex.barco.maicol@gmail.com>
2024-08-07 10:42:34 -0500

    endpoints-separados

---
+++ content-request-node/controllers/userController.js
@@ -0,0 +1,55 @@
+const User = require('../models/User');
+
+exports.getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateUserProfile = async (req, res) => {
+  try {
+    const { username, email, privacy } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { username, email, privacy },
+      { new: true, runValidators: true }
+    ).select('-password');
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.searchUsers = async (req, res) => {
+  try {
+    const { username } = req.query;
+    if (!username) {
+      return res.status(400).json({ message: 'Please provide a username to search' });
+    }
+    const users = await User.find({ 
+      username: { $regex: username, $options: 'i' } 
+    }).select('username email');
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
+exports.deleteAccount = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.user.id);
+    // Aquí deberías también eliminar todas las publicaciones, comentarios, etc. del usuario
+    res.json({ message: 'Account deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
