b02c9383bd4c01f4e191265adbf4239910a2418f
Barcodehub <alex.barco.maicol@gmail.com>
2024-08-07 12:39:59 -0500

    dockerfile

---
+++ content-request-service/src/routes/postRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('../controllers/postController');
+const auth = require('../middlewares/auth');
+
+router.post('/', auth, postController.createPost);
+router.get('/', auth, postController.getPosts);
+router.post('/:postId/share', auth, postController.sharePost);
+router.get('/news-feed', auth, postController.getNewsFeed);
+router.patch('/:postId/privacy', auth, postController.updatePostPrivacy);
+
+router.delete('/:postId', auth, postController.deletePost);
+/* router.delete('/:commentId', auth, commentController.deleteComment);
+router.delete('/:type/:id', auth, likeController.removeLike);
+router.delete('/:friendId', auth, friendController.removeFriend);
+router.delete('/account', auth, userController.deleteAccount); */
+
+router.put('/:postId', auth, postController.updatePost);
+/* router.put('/:commentId', auth, commentController.updateComment);
+router.get('/search', auth, userController.searchUsers); */
+
+module.exports = router;
--- node-jwt-api-js/src/index.js
+++
@@ -1,5 +0,0 @@
-import app from "./app.js";
-
-let PORT = 3001;
-app.listen(PORT);
-console.log("Servidor ejecutando en el puerto", PORT);
