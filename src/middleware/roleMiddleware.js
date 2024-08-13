07c0338f66e2eb4dddb860727ce0fbe306ec4e9f
Barcodehub <alex.barco.maicol@gmail.com>
2024-08-12 22:17:27 -0500

    community-moderatorRol

--- src/middleware/roleMiddleware.js
+++ src/middleware/roleMiddleware.js
@@ -18,16 +18,21 @@ exports.hasRole = (...roles) => {
   };
   
   exports.isCommunityModerator = async (req, res, next) => {
-    if (!req.user) {
-      return res.status(401).json({ message: 'Unauthorized' });
+    try {
+      const communityId = req.params.id;
+      const userId = req.user.id;
+  
+      const community = await Community.findById(communityId);
+      if (!community) {
+        return res.status(404).json({ message: 'Community not found' });
+      }
+  
+      if (community.creator.toString() === userId || community.moderators.includes(userId)) {
+        next();
+      } else {
+        res.status(403).json({ message: 'User is not a moderator of this community' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Error checking moderator status', error: error.message });
     }
-  
-    const communityId = req.params.communityId;
-    const user = await User.findById(req.user._id);
-  
-    if (!user.moderatedCommunities.includes(communityId)) {
-      return res.status(403).json({ message: 'Forbidden' });
-    }
-  
-    next();
   };
--- src/routes/communityRoutes/communities.js
+++ src/routes/communityRoutes/communities.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../../middleware/authMiddleware');
-const { createCommunity, getPublicCommunities, joinCommunity, approveJoinRequest } = require('../../controllers/communityController/communityController');
+const { isCommunityModerator } = require('../../middleware/roleMiddleware');
+const { createCommunity, getPublicCommunities, joinCommunity, approveJoinRequest, assignModerator} = require('../../controllers/communityController/communityController');
 
 
 router.post('/', protect, createCommunity);
 router.get('/public', protect, getPublicCommunities);
 router.post('/join/:id', protect, joinCommunity);
 router.post('/approve/:id/:userId', protect, approveJoinRequest);
+router.post('/:id/moderators', protect, assignModerator);
 
 module.exports = router;
